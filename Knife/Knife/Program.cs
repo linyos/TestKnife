bb89e801c23ff5678d0a0cdd22c638af588081e5
sen <laso85143@gmail.com>
2023-04-15 16:59:56 +0800

    First commit t main branch

---
+++ Knife/Knife/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Knife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
